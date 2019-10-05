308e742e354101a00d12676a2ef0d4fa985922b9
citaUtec <cita@utec.edu.pe>
2019-10-05 18:30:04 -0500

    Merge sort
    Added merge sort algorithm

--- js/index.js
+++ js/index.js
@@ -14,6 +14,7 @@ function RandomArray() {
     document.getElementById("sort").style.display = "inline-block";
     document.getElementById("bsort").style.display = "inline-block";
     document.getElementById("Obsort").style.display = "inline-block";
+    document.getElementById("MergeSort").style.display = "inline-block";
     makeArray();
 
 }
@@ -25,6 +26,7 @@ function CreateArray() {
     document.getElementById("sort").style.display = "inline-block";
     document.getElementById("bsort").style.display = "inline-block";
     document.getElementById("Obsort").style.display = "inline-block";
+    document.getElementById("MergeSort").style.display = "inline-block";
 }
 
 function makeArray() {
@@ -144,4 +146,85 @@ function swapElements(obj1, obj2) {
     temp.parentNode.insertBefore(obj2, temp);
     // remove temporary marker node
     temp.parentNode.removeChild(temp);
+}
+
+async function mergeSortFunction() {
+    array = await mergeSort(array);
+    makeArray();
+}
+
+async function mergeSort(list) {
+
+    makeArray();
+
+    var len = list.length;
+
+    // an array of length == 1 is technically a sorted list
+    if (len === 1) {
+        return list
+    }
+
+    // get mid item
+    var middleIndex = Math.ceil(len / 2);
+
+    // split current list into two: left and right list
+    var leftList = list.slice(0, middleIndex);
+    var rightList = list.slice(middleIndex, len);
+
+    for (var i = 0; i < leftList.length; i++){
+        document.getElementById("bar" + (i + 1)).style.background = "#64dd17";
+        await sleep(500);
+    }
+
+    for (var i = 0; i < rightList.length; i++){
+        document.getElementById("bar" + (list.length - i)).style.background = "#f44336";
+        await sleep(500);
+    }
+
+    console.log(leftList);
+    console.log(rightList);
+
+    leftList = await mergeSort(leftList);
+    rightList = await mergeSort(rightList);
+    return merge(leftList, rightList);
+
+}
+
+// Solve the sub-problems and merge them together
+function merge(leftList, rightList) {
+
+    const sorted = [];
+
+    while (leftList.length > 0 && rightList.length > 0) {
+        const leftItem = leftList[0];
+        const rightItem = rightList[0];
+        if (leftItem > rightItem) {
+            sorted.push(rightItem);
+            rightList.shift()
+        } else {
+            sorted.push(leftItem);
+            leftList.shift()
+        }
+    }
+
+    // if left list has items, add what is left to the results
+    while (leftList.length !== 0) {
+        sorted.push(leftList[0]);
+        leftList.shift()
+    }
+
+    // if right list has items, add what is left to the results
+    while (rightList.length !== 0) {
+        sorted.push(rightList[0]);
+        rightList.shift()
+    }
+
+    for (var i = 0; i < sorted.length; i++){
+        array[i] = sorted[i];
+    }
+
+    makeArray();
+
+    // merge the left and right list
+    return sorted;
 }
